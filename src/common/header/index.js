3c25ab4c0ec0f2a8ba6a2f80e441cd1da27fac62
szy0syz <szy0syz@gmail.com>
2018-09-26 11:39:52 +0800

    ☁️ Update [7-3] 简书header组件布局组件化实现

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 // 这个组件就是一个div标签上面带了些样式
-import { HeaderWrapper, Logo, Nav } from './style'
+import { 
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
+} from './style'
 
 class Header extends Component {
   render() {
     return (
       <HeaderWrapper>
         <Logo />
-        <Nav></Nav>
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavSearch></NavSearch>
+          <Addition>
+            <Button className="writting">写文章</Button>
+            <Button className="reg">注册</Button>
+          </Addition>
+        </Nav>
       </HeaderWrapper>
     )
   }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -23,6 +23,70 @@ export const Logo = styled.a.attrs({
 export const Nav = styled.div`
   width: 960px;
   height: 100%;
+  padding-right: 70px;
+  box-sizing: border-box;
   margin: 0 auto;
-  background: green;
+`
+
+// &.left 如果<NavItem/>组件上有left类名，就向左浮动
+export const NavItem = styled.div`
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color: #333;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+
+  &.active {
+    color: #ea6f5a;
+  }
+`
+// &::placeholder 表示当前组件下的 placeholder
+export const NavSearch = styled.input.attrs({
+  placeholder: '搜索'
+})`
+  height: 38px;
+  width: 160px
+  border: none;
+  outline: none;
+  border-radius: 19px;
+  background: #eee;
+  margin-top: 9px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  font-size: 14px;
+  margin-left: 20px;
+  &::placeholder {
+    color: #999;
+  }
+`
+
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
+`
+
+export const Button = styled.div`
+  float: right;
+  margin-top: 9px;
+  margin-right: 20px;
+  padding: 0 20px;
+  line-height: 38px;
+  border-radius: 19px;
+  border: 1px solid #ec6149;
+  font-size: 14px;
+  &.reg {
+    color: #ec6149;
+  }
+  &.writting {
+    color: #fff;
+    background: #ec6149;
+  }
 `
