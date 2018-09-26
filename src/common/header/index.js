42ae5c19659b47223cf314349014b7c076ead4ae
szy0syz <szy0syz@gmail.com>
2018-09-27 00:14:31 +0800

    🌛 Update [7-11] 热门搜索样式布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -10,10 +10,42 @@ import {
   NavSearch,
   Addition,
   Button,
+  SearchInfo,
+  SearchInfoItem,
+  SearchInfoList,
+  SearchInfoTitle,
+  SearchInfoSwitch,
   SearchWarpper
 } from './style'
 
 const Header = (props) => {
+  const getListArea = (show) => {
+    if (show) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            <SearchInfoItem>行具杯2018征文</SearchInfoItem>
+            <SearchInfoItem>区块链</SearchInfoItem>
+            <SearchInfoItem>小程序</SearchInfoItem>
+            <SearchInfoItem>vue</SearchInfoItem>
+            <SearchInfoItem>毕业</SearchInfoItem>
+            <SearchInfoItem>PHP</SearchInfoItem>
+            <SearchInfoItem>故事</SearchInfoItem>
+            <SearchInfoItem>flutter</SearchInfoItem>
+            <SearchInfoItem>理财</SearchInfoItem>
+            <SearchInfoItem>美食</SearchInfoItem>
+          </SearchInfoList>
+        </SearchInfo>
+      )
+    } else {
+      return null
+    }
+  }
+
   return (
     <HeaderWrapper>
       <Logo />
@@ -34,7 +66,8 @@ const Header = (props) => {
           </CSSTransition>
           <i className={props.isFocused ? 'iconfont focused' : 'iconfont'}>
             &#xe62d;
-            </i>
+          </i>
+          {getListArea(props.isFocused)}
         </SearchWarpper>
       </Nav>
       <Addition>
