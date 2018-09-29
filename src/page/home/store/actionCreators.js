f073db0403425051e9d292896836770fedcfdeba
szy0syz <szy0syz@gmail.com>
2018-09-29 22:52:52 +0800

    🌛 Update [8-7] 异步操作代码拆分优化 自己已经提前做了

--- src/page/home/store/actionCreators.js
+++ src/page/home/store/actionCreators.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { fromJS } from 'immutable'
 import * as actionTypes from './actionTypes'
 
-const changeHomeDate = (data) => ({
+const changeHomeData = (data) => ({
   type: actionTypes.CHANGE_HOME_DATA,
   topicList: fromJS(data.topicList),
   articleList: fromJS(data.articleList),
@@ -13,7 +13,7 @@ export const fetchHomeData = () => {
   return (dispatch) => {
     axios.get('/api/home.json').then(res => {
       const data = res.data.data
-      dispatch(changeHomeDate(data))
+      dispatch(changeHomeData(data))
     })
   }
 }
